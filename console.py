7f7e8a7a184084e517e56d499db1a36e237cdf96
Sumshi <sumayasomow@gmail.com>
2023-09-16 15:13:59 +0000

    fixed bugs in console.py

--- console.py
+++ console.py
@@ -73,8 +73,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
-                            and type(eval(pline)) is dict:
+                    if pline[0] == '{' and pline[-1] == '}'\
+                            and type(eval(pline)) == dict:
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
@@ -114,14 +114,40 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
+        """
+        Create an object of any class
+        with given parameters
+        """
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        # split the args into arguments, where first element is class name
+        args_elements = args.split()
+        class_name = args_elements[0]
+
+        # check if the class name exists in hbnb
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        # parameters: <Class name> <param 1> <param 2> ...
+        param = {}
+        for arg in args_elements[1:]:
+            key_and_value = arg.split('=')
+            if len(key_and_value) != 2:
+                continue
+            # parameter = <key name>=<value>
+            key = key_and_value[0]
+            value = key_and_value[1]
+
+            # strings with quotes
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1]
+
+            # underscores must be replaced with space in keys
+            key = key.replace('-', ' ')
+            param[key] = value
+        # instance with specified parameter
+        new_instance = HBNBCommand.classes[class_name](**param)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +298,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +306,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +345,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
