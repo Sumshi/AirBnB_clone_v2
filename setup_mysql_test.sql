7d575b3bbd46c44056af709eb0884fb0100e4ad9
Sumshi <sumayasomow@gmail.com>
2023-09-16 16:43:05 +0000

    error in setting up test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,7 +8,7 @@ USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- the user hbnb_test should have all priviledges on hbnb_test_db
-GRANT ALL PRIVILEGES ON hbnb_test.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- grant select priviledge on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
