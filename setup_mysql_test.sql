63f2293aa26d4406574857ce969cb20071ec7dc6
Sumshi <sumayasomow@gmail.com>
2023-09-16 16:35:14 +0000

    setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- create or use a test database database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- use the database u just created
+USE hbnb_test_db;
+
+-- create or use an existing user identified by a passowrd
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- the user hbnb_test should have all priviledges on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test.* TO 'hbnb_test'@'localhost';
+
+-- grant select priviledge on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
