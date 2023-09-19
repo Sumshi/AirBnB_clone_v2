91cb179e0fde99805f33e779f6a672f8dbc1146d
Sumshi <sumayasomow@gmail.com>
2023-09-19 07:52:09 +0000

    updated errors on task 6

--- models/place.py
+++ models/place.py
@@ -29,8 +29,7 @@ class Place(BaseModel, Base):
     amenity_ids = []
     reviews = relationship("Review", backref="place")
     amenities = relationship('Amenity', secondary=place_amenity,
-                             viewonly=False,
-                             back_populates='place_amenities')
+                             viewonly=False)
 
     @property
     def reviews(self):
