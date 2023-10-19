55c672bf58b077d6ea312892a128c7b62d689fed
Sumshi <sumayasomow@gmail.com>
2023-10-19 06:34:38 +0000

    hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    display Hello HBNB!
+    Returns:
+        [str]: Hello HBNB!
+    run the script on local server ip address and port 5000
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display HBNB
+    Returns:
+        [str]: HBNB
+    run the script on local server ip address and port 5000
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
