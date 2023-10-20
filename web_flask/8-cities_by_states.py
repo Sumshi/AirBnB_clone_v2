69669cefbe9eb28c4408cd80340a63d329ce8e54
Sumshi <sumayasomow@gmail.com>
2023-10-20 09:52:50 +0000

    included modules

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,7 +3,9 @@
 
 from flask import Flask
 from flask import render_template
-from models import storage, State
+from models import storage
+from models.state import State
+from models.city import City
 
 app = Flask(__name__, template_folder='templates')
 
